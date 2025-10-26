220d30492e2d2f46c71f204881841377bc2bbe3a
daniseifeddine <dany.a.seifeddine@gmail.com>
2025-10-26 10:07:58 +0200

    Add new build assets for various components and layouts
    - Introduced multiple new JavaScript files for components such as ActionLayout, AdminLayout, and various UI elements, enhancing the frontend structure and functionality.
    - Updated the manifest file to include the new assets, ensuring they are properly referenced in the build process.
    - Removed outdated build assets to streamline the project and improve maintainability.

--- public/build/assets/Card.vue_vue_type_script_setup_true_lang-DQ_cJ84B.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./createLucideIcon-gZqVR3e7.js";import{d as o,c as n,o as t,r as c,x as d,u as l}from"./app-D8Tikvxa.js";const f=o({__name:"Card",props:{class:{}},setup(s){const r=s;return(a,m)=>(t(),n("div",{class:d(l(e)("rounded-lg border bg-card text-card-foreground shadow-sm",r.class))},[c(a.$slots,"default")],2))}});export{f as _};
